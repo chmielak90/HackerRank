5ac47ebb3c235adcc06ab2a03503b337c340f13c
Michal Chmielewski <90.michal.chmielewski@gmail.com>
2017-11-21 14:55:02 +0100

    Task from day 17 - done

---
+++ Day_17/Day 17: More Exceptions.py
@@ -0,0 +1,20 @@
+### Task start here
+
+class Calculator:
+    def power(self, n, p):
+        if n >= 0 and p >= 0:
+            return pow(n, p)
+        else:
+            raise ValueError('n and p should be non-negative')
+
+### Task end here
+
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)
